a86d598e231f709dee2f81c99d603fb4cfd0d7a1
lirit <zzk@live.com>
2020-08-09 11:43:19 +0800

    up

---
+++ surge/Rules/Media/TwMedia.list.js
@@ -0,0 +1,16 @@
+# common TaiWan streaming
+# Bahamut
+USER-AGENT,Anime*
+DOMAIN-SUFFIX,bahamut.com.tw
+DOMAIN-SUFFIX,gamer.com.tw
+DOMAIN-SUFFIX,hinet.net
+# > KKBOX
+DOMAIN-SUFFIX,kkbox.com
+DOMAIN-SUFFIX,kkbox.com.tw
+DOMAIN-SUFFIX,kfs.io
+# > KKTV
+USER-AGENT,KKTV*
+USER-AGENT,com.kktv*
+DOMAIN-SUFFIX,kktv.me
+DOMAIN-SUFFIX,kktv.com.tw
+DOMAIN,kktv-theater.kk.stream
